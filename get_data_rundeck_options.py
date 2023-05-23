6b28d208c29214bc75a47bc14e5b0618896788ab
Rafael García Álvarez <vattenbit@gmail.com>
2023-05-23 19:40:10 +0200

    resize size of the number

--- get_data_rundeck_options.py
+++ get_data_rundeck_options.py
@@ -29,7 +29,7 @@ for data in response.get(args.data):
             value = data.get('slug')
             ord += 1
         case 'images':
-            key = "{:02}".format(ord) + " - {distribution} | {name}".format(**data)
+            key = "{:03}".format(ord) + " - {distribution} | {name}".format(**data)
             value = data.get('slug')
             ord += 1
     output_json[key] = value
