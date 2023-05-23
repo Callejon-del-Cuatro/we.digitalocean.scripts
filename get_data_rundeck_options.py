a37a1fa54e32ecf5af20523b87b13bfe9b6760ea
Rafael García Álvarez <vattenbit@gmail.com>
2023-05-23 19:24:08 +0200

    Add getter for images in the script

--- get_data_rundeck_options.py
+++ get_data_rundeck_options.py
@@ -14,7 +14,7 @@ parser.add_argument("-o","--output", required=True, help="Path for create file")
 args = parser.parse_args()
 
 headers = {'content-type': 'application/json', 'Authorization': 'Bearer {0}'.format(args.token)}
-req = requests.get(URL_API+"/"+args.data + "?per_page=100", headers=headers)
+req = requests.get(URL_API+"/"+args.data + "?per_page=1000", headers=headers)
 response = req.json()
 
 output_json = {}
@@ -26,6 +26,10 @@ for data in response.get(args.data):
             value = data.get('slug')
         case 'sizes':
             key = "{:02}".format(ord) + " - {description} | Mem: {memory} Mb | CPUs: {vcpus} | Disk: {vcpus} Gb | Trans: {transfer} Tb/mo | Price: {price_monthly} €/mo".format(**data)
+            value = data.get('slug')
+            ord += 1
+        case 'images':
+            key = "{:02}".format(ord) + " - {name}".format(**data)
             value = data.get('slug')
             ord += 1
     output_json[key] = value
